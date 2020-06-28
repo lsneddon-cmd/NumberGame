1c0d9c82da26de6ae9aaacb07d1cc15e7643793b
Lewis Sneddon <lewis@lsneddon.com>
2020-06-28 11:23:50 +0100

    Project Setup
    Setup basic working project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 NumberGame
+
+A Command Line Math game that requires the user to enter 3 numbers given their sum and product
---
+++ numbergame.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <ctime>
+
+bool PlayGame(int Level);
+void PrintIntroduction(int Difficulty);
+
+int main()
+{
+    srand(time(NULL));
+
+    int LevelDifficulty = 1;
+    const int MaxLevel = 5;
+    while (LevelDifficulty <= MaxLevel)
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear();
+        std::cin.ignore();
+
+        if (bLevelComplete)
+        {
+            ++LevelDifficulty;
+        }
+    }
+    std::cout << "Congratulations! You've won the game!\n";
+    return 0;
+}
+
+void PrintIntroduction(int Difficulty)
+{
+    std::cout << "Level: " << Difficulty;
+}
+
+bool PlayGame(int Level)
+{
+    PrintIntroduction(Level);
+
+    // Declare a 3 number code
+    int CodeA = rand() % Level + Level;
+    int CodeB = rand() % Level + Level;
+    int CodeC = rand() % Level + Level;
+
+    int CodeSum = CodeA + CodeB + CodeC;
+    int CodeProduct = CodeA * CodeB * CodeC;
+
+    std::cout << std::endl;
+    std::cout << "+ There are 3 numbers in the code" << std::endl;
+    std::cout << "+ The codes add up to: " << CodeSum << std::endl;
+    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
+
+    int GuessA, GuessB, GuessC;
+
+    std::cout << "Enter guess: ";
+    std::cin >> GuessA >> GuessB >> GuessC;
+
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct)
+    {
+        std::cout << "\nWell done!\nMoving to the next level\n";
+        return true;
+    }
+    else
+    {
+        std::cout << "\nYou entered the wrond code ... try again\n";
+        return false;
+    }
+}
